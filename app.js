0bf5cea8c2132d8b689e5e5045267a6fac56fd3c
Troy Dorman <troydorman@gmail.com>
2021-09-20 19:50:43 -0600

    created the subscribe post route by delivering a payload and sending the notification

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const webpush = require("web-push");
 const path = require("path");
 const app = express();
 
-const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
-const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+const publicVapidKey = process.env.VAPID_PUBLIC_KEY;
+const privateVapidKey = process.env.VAPID_PRIVATE_KEY;
 
 webpush.setVapidDetails(
   "mailto: troydorman@gmail.com",
@@ -20,6 +20,20 @@ app.use(express.json({ extended: false }));
 const port = process.env.PORT || 5000;
 
 // create post subscribe route
-app.post("/subscribe", (req, res) => {});
+app.post("/subscribe", (req, res) => {
+  // get push subscription object
+  const subscription = req.body;
+
+  // send 201 status - resource created
+  res.status(201).json({});
+
+  // create payload
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  // pass object into sendNotification
+  webpush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.error(err));
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
