b00faee294f89bffa467e9ecc805edadd7387cdf
Troy Dorman <troydorman@gmail.com>
2021-09-20 18:32:25 -0600

    created subscribe post route

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ app.use(express.json({ extended: false }));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// create post subscribe route
+app.post("/subscribe", (req, res) => {});
 
+app.listen(port, () => console.log(`Server started on port ${port}`));
