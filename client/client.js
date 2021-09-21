f31ce02bcc8201eee29ba22d35cc108ac131d967
Troy Dorman <troydorman@gmail.com>
2021-09-20 20:35:06 -0600

    added function to convert base64 to int 8 bit array.  registered our push notification

--- client/client.js
+++ client/client.js
@@ -10,11 +10,33 @@ if ("serviceWorker" in navigator) {
 
 // register service worker, register push, send push
 async function send() {
+  // register service worker
   console.log("registering service worker...");
-
   const register = await navigator.serviceWorker.register("/worker.js", {
     scope: "/",
   });
-
   console.log("service worker registered!");
+
+  // register push
+  console.log("registering push...");
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+  });
+  console.log("push registered!");
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
